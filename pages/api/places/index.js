b8ca6d5cff436ca56082753db377f0ef61405a84
Kevin Koriath <kevin.koriath@gmx.de>
2023-03-21 15:46:43 +0100

    add something

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,25 @@
-import { places } from '../../../lib/db';
+/* import { places } from "../../../lib/db"; */
+import Place from "../../../db/models/Places.js";
+import dbConnect from "../../../db/connect.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+
+      response.status(201).json({ status: "Joke created" });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+  response.status(400).json("Not found");
 }
--- pages/create.js
+++ pages/create.js
@@ -1,18 +1,39 @@
-import Link from 'next/link.js';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import Form from '../components/Form.js';
-import { StyledLink } from '../components/StyledLink.js';
+import Link from "next/link.js";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import Form from "../components/Form.js";
+import { StyledLink } from "../components/StyledLink.js";
+import useSWR from "swr";
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
 
+const defaultData = {
+  name: "",
+  image:
+    "https://images.unsplash.com/photo-1553547274-0df401ae03c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGF1c3NlbmFsc3RlciUyMGhhbWJ1cmd8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+  location: "",
+  mapURL:
+    "https://www.google.com/maps/place/Elbphilharmonie+Hamburg/@53.543085,9.9859608,15.47z/data=!4m5!3m4!1s0x47b18f066770c44f:0xb2e4ab2a68984286!8m2!3d53.5413297!4d9.9841308",
+  description: "",
+};
+
 export default function CreatePlacePage() {
   const router = useRouter();
 
-  function addPlace(place) {
-    console.log('Place added (but not really...)');
+  async function addPlace(place) {
+    const response = await fetch("/api/places", {
+      method: "POST",
+      body: JSON.stringify(place),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      await response.json();
+      router.push("/");
+    } else {
+      console.error(response.status);
+    }
   }
 
   return (
@@ -21,7 +42,7 @@ export default function CreatePlacePage() {
       <Link href="/" passHref legacyBehavior>
         <StyledBackLink>back</StyledBackLink>
       </Link>
-      <Form onSubmit={addPlace} formName={'add-place'} />
+      <Form onSubmit={addPlace} formName={"add-place"} />
     </>
   );
 }
